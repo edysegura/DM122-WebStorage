da48589a2e9b9309f78d091188e694e3792f7559
Edy Segura <edysegura@gmail.com>
2025-10-25 10:19:26 -0300

    chore: integrate with html and add try/catch

--- cache-api/app.js
+++ cache-api/app.js
@@ -2,22 +2,34 @@ const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
   console.log(`👁️ [app.js] button clicked`);
-  // fetchFromNetwork("https://nonexist/api/v2/pokemon/150");
-  fetchFromNetwork("https://httpbin.org/status/500");
+  fetchFromNetwork("https://pokeapi.co/api/v2/pokemon/150").then((data) =>
+    showData(data)
+  );
 });
 
 async function fetchFromNetwork(url) {
   console.log(`👁️ [app.js] fetching data...`);
-  const response = await fetch(url);
-  if (!response.ok) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      console.error(
+        `Error on requesting ${url} with status code ${response.status}`
+      );
+      return;
+    }
     console.log(
-      `Error on requesting ${url} with status code ${response.status}`
+      `👁️ [app.js] response content-type`,
+      response.headers.get("content-type")
     );
-    return;
+    return response.json();
+  } catch (error) {
+    console.error(`👁️ [app.js] network failed`);
   }
-  console.log(
-    `👁️ [app.js] response content-type`,
-    response.headers.get("content-type")
-  );
-  return response.json();
+}
+
+function showData(data) {
+  console.log(`👁️ [app.js] show data`, data);
+  const img = document.createElement("img");
+  img.src = data.sprites.front_default;
+  document.body.appendChild(img);
 }
