8fcf630b4a872a8a1916696483e27345752bef67
Edy Segura <edysegura@gmail.com>
2025-10-25 09:35:46 -0300

    chore: trying out fetch api

--- cache-api/app.js
+++ cache-api/app.js
@@ -1,1 +1,23 @@
-console.log(`👁️ [app.js] hi 👋`);
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+  console.log(`👁️ [app.js] button clicked`);
+  // fetchFromNetwork("https://nonexist/api/v2/pokemon/150");
+  fetchFromNetwork("https://httpbin.org/status/500");
+});
+
+async function fetchFromNetwork(url) {
+  console.log(`👁️ [app.js] fetching data...`);
+  const response = await fetch(url);
+  if (!response.ok) {
+    console.log(
+      `Error on requesting ${url} with status code ${response.status}`
+    );
+    return;
+  }
+  console.log(
+    `👁️ [app.js] response content-type`,
+    response.headers.get("content-type")
+  );
+  return response.json();
+}
